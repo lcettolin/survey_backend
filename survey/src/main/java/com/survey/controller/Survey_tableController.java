932b8ae1b344b6338c87187e637235b6422fa8f7
lcettolin <lorenzo_cettolin@outlook.com>
2022-11-10 09:05:19 +0100

    mattino

---
+++ survey/src/main/java/com/survey/controller/Survey_tableController.java
@@ -0,0 +1,2 @@
+package com.survey.controller;public class Survey_tableController {
+}
---
+++ survey/src/main/java/com/survey/model/Survey_table.java
@@ -0,0 +1,10 @@
+package com.survey.model;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "question")
+public class Survey {
+}
